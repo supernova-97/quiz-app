9bbc6333ec4c6574ee44e252acf29e4c70f4f4e3
Felix Buyni <felix.buyni@outlook.de>
2023-02-14 09:06:20 +0100

    added bookmark & profile page, made navbar & buttons functional

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,20 @@
-const getCardAnswer = document.querySelector('[data-js="card__answer"]');
-const showAnswer = document.querySelector('[data-js="card__answer-button"]');
+const getCardAnswer1 = document.querySelector('[data-js="card__answer1"]');
+const showAnswer1 = document.querySelector('[data-js="card__answer-button1"]');
 
-showAnswer.addEventListener("click", () => {
-  getCardAnswer.classList.toggle("card__answer--visible");
+showAnswer1.addEventListener("click", () => {
+  getCardAnswer1.classList.toggle("card__answer--visible");
+});
+
+const getCardAnswer2 = document.querySelector('[data-js="card__answer2"]');
+const showAnswer2 = document.querySelector('[data-js="card__answer-button2"]');
+
+showAnswer2.addEventListener("click", () => {
+  getCardAnswer2.classList.toggle("card__answer--visible");
+});
+
+const getCardAnswer3 = document.querySelector('[data-js="card__answer3"]');
+const showAnswer3 = document.querySelector('[data-js="card__answer-button3"]');
+
+showAnswer3.addEventListener("click", () => {
+  getCardAnswer3.classList.toggle("card__answer--visible");
 });
