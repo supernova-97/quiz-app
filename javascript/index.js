d9ff624e27ccc553cb720722149d0120ef7caf94
Felix Buyni <felix.buyni@outlook.de>
2023-02-17 16:16:41 +0100

    made js more compact and usable, changed css structure,

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,18 @@
-const getCardAnswer1 = document.querySelector('[data-js="card__answer1"]');
-const showAnswer1 = document.querySelector('[data-js="card__answer-button1"]');
+const answerButtons = document.querySelectorAll(".card__answer-button");
+const answers = document.querySelectorAll(".card__answer");
 
-showAnswer1.addEventListener("click", () => {
-  getCardAnswer1.classList.toggle("card__answer--visible");
-  textChange();
+answerButtons.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    answers[index].classList.toggle("card__answer--visible");
+    textChange(index);
+  });
 });
 
-function textChange() {
-  if (showAnswer1.innerHTML === "Show Answer") {
-    showAnswer1.innerHTML = "Hide Answer";
+function textChange(buttonIndex) {
+  console.log(buttonIndex);
+  if (answerButtons[buttonIndex].innerHTML === "Show Answer") {
+    answerButtons[buttonIndex].innerHTML = "Hide Answer";
   } else {
-    showAnswer1.innerHTML = "Show Answer";
+    answerButtons[buttonIndex].innerHTML = "Show Answer";
   }
 }
-
-const getCardAnswer2 = document.querySelector('[data-js="card__answer2"]');
-const showAnswer2 = document.querySelector('[data-js="card__answer-button2"]');
-
-showAnswer2.addEventListener("click", () => {
-  getCardAnswer2.classList.toggle("card__answer--visible");
-});
-
-const getCardAnswer3 = document.querySelector('[data-js="card__answer3"]');
-const showAnswer3 = document.querySelector('[data-js="card__answer-button3"]');
-
-showAnswer3.addEventListener("click", () => {
-  getCardAnswer3.classList.toggle("card__answer--visible");
-});
